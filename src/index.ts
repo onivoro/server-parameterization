b60331497839e473adbfc9aab622705b7be59b05
Lee Norris <lee.norris@arcadia.io>
2020-09-29 23:01:57 -0400

    fix: import -> export

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
-import { Arg } from './lib/arg';
+export { Arg } from './lib/arg';
 export { Env } from './lib/env';
 export { Parameter } from './lib/parameter';
-
-class All extends Arg<string> { id = () => 'things' }
-
-console.log(new All().value())
